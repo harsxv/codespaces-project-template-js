2718615b2932e6fc6928704e1753983daed2116a
Harry Suryapambagya <harsxv@gmail.com>
2023-11-20 11:07:39 +0000

    Getting started with GitHub Codepilot

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Harry Suryapambagya",
+  title: "System Engineer and Project Manager",
+  email: "mail@harry.coffee",
+  gitHub: "harsxv",
+  instagram: "suryapambagya",
+  linkedIn: "harsxv",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "harsxv",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/laptop-3076957_1280.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
@@ -31,12 +31,10 @@ const description =
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Cloud Computing",
+  "Infrastructure as Code",
+  "Linux Administration",
+  "Project Management",
 ];
 
 /**
